1e71a19ff3fc3da726373cefeb4c3256dbf6c630
Amlan Alok <amlanalok@gmail.com>
2021-12-05 15:59:22 -0600

    basic chat implementation done

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./Chat.css";
+import ChatBox from '../ChatBox/ChatBox';
 
 function Chat(props) {
 
     const [chats, setChats] = useState([])
     const [userId, setUserId] = useState(props.userId)
+    const [receiverChatBoxData, setReceiverChatBoxData] = useState([])
+    const [showChatBox, setShowChatBox] = useState(false)
 
     useEffect(() => {
 
-        const chatUrl = 'http://34.127.76.90:8080/chats/userId/'+userId
+        const chatUrl = 'http://34.127.76.90:8080/chats/userId/' + userId
         console.log('Calling Chat API -> ' + chatUrl)
 
         axios.get(chatUrl).then(res => {
@@ -24,11 +28,47 @@ function Chat(props) {
 
     }, [])
 
+    function openChatBox(receiver) {
+
+        // setReceiverChatBoxData(receiver)
+        // setShowChatBox(!showChatBox)
+        console.log('key =', receiver.receiverId)
+        let x = receiver
+        setReceiverChatBoxData(x)
+        // if (!showChatBox){
+        //     setShowChatBox(true)
+        // }
+        setShowChatBox(!showChatBox)
+        setShowChatBox(!showChatBox)
+    }
+
     return (
         <div>
-            Chat Area
+            <div className="row">
+                <div className="col-sm-6 ac">
+
+                    Chat List
+
+                    {chats && chats.map(receiver => {
+
+                        return (
+                            // {/* // <div> */}
+                            // {/* // <button key = {receiver.receiverId} onClick = {openChatBox(receiver)}>  // This line is an issue */}
+                            // {/* // https://stackoverflow.com/questions/59304283/error-too-many-re-renders-react-limits-the-number-of-renders-to-prevent-an-in?rq=1 */}
+                            <button className="chat-title-design" key={receiver.receiverId} onClick={() => openChatBox(receiver)}> {receiver.firstName} {receiver.lastName}</button>
+                        )
+                    })}
+
+                </div>
+
+                <div className="col-sm-6 bc">
+                    {showChatBox && <ChatBox receiverChatBoxData={receiverChatBoxData} userId={userId} />}
+                    {/* {<ChatBox receiverChatBoxData={receiverChatBoxData} userId={userId} />} */}
+                </div>
+            </div>
+
         </div>
     )
-} 
+}
 
 export default Chat;
---
+++ src/pages/ChatBox/ChatBox.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react'
+import "./ChatBox.css"; 
+import { io } from 'socket.io-client'
+
+let ip_address = '34.127.76.90';
+let socket_port = '9000';
+let socket = io(ip_address + ':' + socket_port);
+
+function ChatBox(props) {
+
+    const [newChatMessage,setNewChatMessage] = useState('')
+    const [chatMessageList, setChatMessageList] = useState(props.receiverChatBoxData.chatMessages)
+
+    socket.on('sendChatToReceiverFromSender', (receivedChatMsg, receiverId, senderId) => {
+        console.log('inside sendChatToReceiverFromSender')
+        console.log(receivedChatMsg, receiverId, senderId)
+        if (props.userId == receiverId && props.receiverChatBoxData.receiverId == senderId){
+            console.log('inside if')
+            let numOfChats = chatMessageList.length
+            console.log('numOfchats =', numOfChats)
+            let lastChat = chatMessageList[numOfChats-1];
+            console.log(lastChat)
+            let previousId = lastChat.id
+            console.log('previousId =', previousId)
+            let newDisplayChatObj = {
+                id: previousId+1,
+                senderId: senderId,
+                receiverId: receiverId,
+                message: receivedChatMsg
+            }
+            console.log(newDisplayChatObj)
+            let newDisplayChatMsgArray = chatMessageList.concat(newDisplayChatObj);
+            setChatMessageList(newDisplayChatMsgArray)
+
+        }
+        else{
+            console.log('inside else')
+        }
+    })
+
+    function sendChatMessageToReceiver(message, senderUserId, receiverUserId) {
+        socket.emit('sendMsgFromSenderToReceiver', message, senderUserId, receiverUserId);
+    }
+
+    useEffect(() => {
+        console.log(props.receiverChatBoxData)
+        console.log(chatMessageList)
+    })
+
+    function readNewChatMessage(e) {
+        setNewChatMessage(e.target.value)
+    }
+
+    function addNewChatToList(){
+        
+        let numOfChats = chatMessageList.length
+        console.log('numOfchats =', numOfChats)
+        let lastChat = chatMessageList[numOfChats-1];
+        console.log(lastChat)
+        let previousId = lastChat.id
+        console.log('previousId =', previousId)
+        let newChatObj = {
+            id: previousId+1,
+            senderId: props.userId,
+            receiverId: props.receiverChatBoxData.receiverId,
+            message: newChatMessage
+        }
+        console.log(newChatObj)
+        let newChatMsgArray = chatMessageList.concat(newChatObj);
+        setChatMessageList(newChatMsgArray)
+        sendChatMessageToReceiver(newChatObj.message, newChatObj.senderId, newChatObj.receiverId)
+        setNewChatMessage('')
+    }
+
+    return (
+        <div>
+            <div>
+                <div>
+                    {props.receiverChatBoxData.firstName} {props.receiverChatBoxData.lastName}
+                </div>
+                <div>
+                    {chatMessageList && chatMessageList.map(chat => {
+                        return (
+                            props.userId == chat.senderId ? (<div className = "chat-sender-position" key = {chat.id}>{chat.message}</div>) : 
+                            (<div className = "chat-receiver-position" key = {chat.id}>{chat.message}</div>)
+                        )
+                    }
+
+                    )}
+                </div>
+                
+                <div>
+                    <input onChange={readNewChatMessage} value={newChatMessage} type="text"></input>
+                    <button className="btn btn-primary" onClick = {addNewChatToList}>Send</button>
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default ChatBox;
